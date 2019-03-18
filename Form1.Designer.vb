a972a194b4c7fa13fb9272d9c658f9c1fecaa878
zijing <zijing@users.noreply.github.com>
2019-03-18 22:54:07 +0800

    upload files

---
+++ Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Close = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.Close, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Close
+        '
+        Me.Close.BackColor = System.Drawing.Color.Transparent
+        Me.Close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Close.Location = New System.Drawing.Point(881, 12)
+        Me.Close.Name = "Close"
+        Me.Close.Size = New System.Drawing.Size(30, 33)
+        Me.Close.TabIndex = 0
+        Me.Close.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("黑体", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(152, 202)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(618, 97)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Hello World!"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(923, 500)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Close)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Hello World!"
+        CType(Me.Close, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Close As PictureBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,64 @@
+﻿Public Class Form1
+
+    '移动窗口
+    Private IsFormBeingDragged As Boolean = False
+    Private MouseDownX As Integer
+    Private MouseDownY As Integer
+
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseDown
+
+        If e.Button = MouseButtons.Left Then
+            IsFormBeingDragged = True
+            MouseDownX = e.X
+            MouseDownY = e.Y
+        End If
+    End Sub
+
+    Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseUp
+
+        If e.Button = MouseButtons.Left Then
+            IsFormBeingDragged = False
+        End If
+    End Sub
+
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseMove
+
+        If IsFormBeingDragged Then
+            Dim temp As Point = New Point()
+
+            temp.X = Me.Location.X + (e.X - MouseDownX)
+            temp.Y = Me.Location.Y + (e.Y - MouseDownY)
+            Me.Location = temp
+            temp = Nothing
+        End If
+    End Sub
+
+
+
+    '关闭按钮
+
+    '设置按钮贴图
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Close.Image = My.Resources.close_0
+    End Sub
+    '单击按钮退出
+    Private Sub Close_Click(sender As Object, e As EventArgs) Handles Close.Click
+        End
+    End Sub
+    '按钮选中时贴图
+    Private Sub Close_MouseMove(sender As Object, e As MouseEventArgs) Handles Close.MouseMove
+        Close.Image = My.Resources.close_1
+    End Sub
+
+    '按钮按下时贴图
+    Private Sub Close_MouseDown(sender As Object, e As MouseEventArgs) Handles Close.MouseDown
+        Close.Image = My.Resources.close_2
+    End Sub
+    '按钮移开时恢复贴图
+    Private Sub Close_MouseLeave(sender As Object, e As EventArgs) Handles Close.MouseLeave
+        Close.Image = My.Resources.close_0
+    End Sub
+
+
+End Class
+
