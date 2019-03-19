e65a72e399fa48f7942b114660beff0e9e0c2c5a
zijing <zijing@users.noreply.github.com>
2019-03-19 12:56:16 +0800

    update files

--- Form1.vb
+++ Form1.vb
@@ -1,6 +1,30 @@
 ﻿Public Class Form1
 
-    '移动窗口
+
+    '窗口淡入淡出函数
+    Private Sub FormFade(ByVal FType)
+        Select Case FType
+            Case ("in")
+                Dim FadeCount As Integer
+                For FadeCount = 10 To 90 Step 10
+                    Me.Opacity = FadeCount / 100
+                    Me.Refresh()
+                    Threading.Thread.Sleep(50)
+                Next
+            Case ("out")
+                Dim FadeCount As Integer
+                For FadeCount = 90 To 10 Step -10
+                    Me.Opacity = FadeCount / 100
+                    Me.Refresh()
+                    Threading.Thread.Sleep(50)
+                Next
+        End Select
+        Me.Opacity = 99
+    End Sub
+
+
+
+    '移动窗口函数
     Private IsFormBeingDragged As Boolean = False
     Private MouseDownX As Integer
     Private MouseDownY As Integer
@@ -35,14 +59,17 @@
 
 
 
-    '关闭按钮
+    '事件
 
-    '设置按钮贴图
+    '设置按钮贴图，淡入
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Me.DoubleBuffered = True '采用缓冲，解决窗体闪烁问题
+        FormFade("in") '窗体打开时淡入
         Close.Image = My.Resources.close_0
     End Sub
     '单击按钮退出
     Private Sub Close_Click(sender As Object, e As EventArgs) Handles Close.Click
+        FormFade("out")
         End
     End Sub
     '按钮选中时贴图
@@ -59,6 +86,9 @@
         Close.Image = My.Resources.close_0
     End Sub
 
-
+    '关闭窗口时淡出
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        FormFade("out")
+    End Sub
 End Class
 
